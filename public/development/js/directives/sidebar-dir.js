1ad9161c3642005761242e70554b927e399891c4
Larry Clem <larryvincentclem@gmail.com>
2016-04-22 13:18:03 -0500

    sidebar directive toggle functionality working, active units container is responsive, raleway font imported

--- public/development/js/directives/sidebar-dir.js
+++ public/development/js/directives/sidebar-dir.js
@@ -1,8 +1,14 @@
 angular.module('dataGrow')
 	.directive('sideBar', function() {
 		return {
-
-		templateUrl: '../../../development/partials/sidebar.html'
+		restrict: 'AE',
+		templateUrl: '../../../development/partials/sidebar.html',
+		link: function (scope, elem, attrs) {
+			$("#menu-toggle").click(function(e) {
+			    e.preventDefault();
+			    $("#wrapper").toggleClass("toggled");
+			});
+		}
 
 	}
 });
