93bc6a0930ffd819f93cefd4c7a1e69360509b79
Keith Holmes <keithholmes248@gmail.com>
2016-04-22 14:19:45 -0500

    add crud functions to unit ctrl

--- server/routes/unitCtrl.js
+++ server/routes/unitCtrl.js
@@ -1,25 +1,82 @@
 'use strict';
 
 let Archive = require('../models/ArchiveUnitSchema'),
-    User = require('../models/UserCollection'),
+    UserCollection = require('../models/UserCollection.js'),
     Unit = require('../models/UnitSchema');
+
+
+let userCollectionId = "571a73b7b1fb75f953934e66";
 
 module.exports = {
     
     getUnitsList: ( req, res ) => {
-
         
-        User.find({}, ( err , UserCollection ) => {
-            
-            if (err) {
-                return res.status(500).send(err);
-            }
-            
-            res.send(UserCollection);
-            
-        })
+        UserCollection
+            .findOne( {}, "units" )
+            .then( function(err, UnitsList) {                
+                if (err) 
+                   res.status(500).send(err);
+                else
+                   res.send(UnitsList);                
+            });
+    },
+
+    createUnit: function(req, res) {
+        Unit.create(req.body)
+            .then(function(err, newUnit) {
+                UserCollection.findByIdAndUpdate(
+                    userCollectionId,    //query
+                    {$push: {units: newUnit}}, //push new unit
+                    {safe: true, upsert: true}, //options
+                    function(err, UserCollection) {
+                        if(err) 
+                            res.status(300).send(err);
+                        else
+                            res.status(201).send(UserCollection);
+                    }
+                )
+            })
+    },
+
+    getUnit: function(req, res) {
+        Unit.findById(req.body)
+            .then(function(err, queriedUnit) {
+                if(err)
+                    res.status(300).send(err);
+                else
+                    res.status(201).send(queriedUnit);
+            })
+    },
+
+    deleteUnit: function(req, res) {
+        Unit.findByIdandRemove(req.body)
+            .then(function(err, removedUnit) {
+                if(err)
+                    res.status(300).send(err);
+                else
+                    res.status(201).send(JSON.stringified("Unit Removed"));
+            })
     }
-    
+
+
+
+
+
+
+
+
+
+
+    createCollection: function(req, res) {
+        console.log(UserCollection);
+
+        UserCollection.create(req.body).then(function(err, Collection) {
+            if(err) 
+                res.status(300).send(err);
+            else
+                res.status(200).send(Collection);
+        })     
+    }
     
     
 };
