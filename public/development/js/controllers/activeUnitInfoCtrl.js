861db36cebc07167ae5b590dd9f2c587dc011f1f
Justin Bennett <fortitude1981@gmail.com>
2016-04-25 15:28:44 -0500

    added data parameters

--- public/development/js/controllers/activeUnitInfoCtrl.js
+++ public/development/js/controllers/activeUnitInfoCtrl.js
@@ -8,17 +8,36 @@ angular.module('dataGrow')
   $scope.ActiveUnits = [];
   $scope.UnitId;
   $scope.Data;
- 
- // UnitId.Data()
- // 	.success(function(Data) {
- // 		this.Data = Data;
- // 	});
 
   function getUnitInfo() {
     $scope.UnitId = function() {
   	 $scope.Data.push($scope.UnitId)
-        return UnitId.data
+        return UnitId.data;
     };
+  }
+
+  function getUnitWaterTemp() {
+    $scope.UnitId = function() {
+      return UnitId.waterTemp;
+    }
+  }
+
+  function getUnitAirTemp() {
+    $scope.UnitId = function() {
+      return UnitId.airTemp;
+    }
+  }
+
+  function getUnitHumidity() {
+    $scope.UnitId = function() {
+      return UnitId.humidity;
+    }
+  }
+
+  function getUnitLight() {
+    $scope.UnitId = function() {
+      return UnitId.light;
+    }
   }
   // ws.on('info', function(data) {
   //    $scope.info.push(data);
