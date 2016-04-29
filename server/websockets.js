674799113d123598365df7c2c301ea75c2cd413a
nvankems <nvankems@yahoo.com>
2016-04-28 20:15:30 -0500

    heroku testing

--- server/websockets.js
+++ server/websockets.js
@@ -28,3 +28,43 @@ wss.on('connection', function connection(ws) {
   });
     
 });
+//
+// let Unit = require('./models/UnitSchema')
+//
+// setInterval (function() {
+//
+//
+//
+//   function pushToDB(avgData) {
+//     Unit.findById('571fd58d5110c15712568a0a', function(err, queriedUnit) {
+//       //get all arrays
+//       var dayArr = queriedUnit.day;
+//       var hourArr = dayArr[dayArr.length-1].hour;
+//       var dataArr = hourArr[hourArr.length-1].data;
+//
+//       //push new data to data arr
+//       dataArr.push(avgData);
+//
+//       //if hour has 4 data objects add new hour
+//       if (dataArr.length >= 4) {
+//         hourArr.push({ data: [ ] });
+//       }
+//
+//       //if day has 24 hour objects add new day
+//       if (hourArr.length >= 24) {
+//         dayArr.push({  hour: [{   data: [ ]   }]   });
+//       };
+//       console.log(queriedUnit);
+//       queriedUnit.save();
+//     });
+//
+//   }
+//
+//
+//   pushToDB( {waterTemp : Math.floor(Math.random() * (80 - 50)) + 50,
+//   airTemp : Math.floor(Math.random() * (100 - 35)) + 35,
+//   humidity : Math.floor(Math.random() * 100),
+//   light : Math.floor(Math.random() * 1001) } );
+//
+//
+// },750);
